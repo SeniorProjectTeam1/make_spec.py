d94a109ddc2f765fdf324dac73969edf5999a18d
RyanR1614 <rrapa@students.kennesaw.edu>
2025-11-20 12:51:07 -0500

    Open spectrogram after generation in run_wts.bat

--- run_wts.bat
+++ run_wts.bat
@@ -1,3 +1,6 @@
 @echo off
 REM Run make_spec for the default wts.wav in the user's Music folder
 python "%~dp0\make_spec.py" "C:\Users\rusty\Music\wts.wav"
+
+REM Open the generated spectrogram in the default image viewer
+start "" "C:\Users\rusty\Music\wts_spectrogram.png"
